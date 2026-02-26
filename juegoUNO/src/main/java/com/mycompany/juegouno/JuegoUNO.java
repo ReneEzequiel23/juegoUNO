245b9d671bc830b9f1b1ae52ab81124753a27334
ReneEzequiel23 <150867485+ReneEzequiel23@users.noreply.github.com>
2026-02-25 22:26:02 -0700

    Add files via upload

---
+++ juegoUNO/src/main/java/com/mycompany/juegouno/JuegoUNO.java
@@ -0,0 +1,16 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ */
+
+package com.mycompany.juegouno;
+
+/**
+ *
+ * @author renee
+ */
+public class JuegoUNO {
+
+    public static void main(String[] args) {
+        System.out.println("Hello World!");
+    }
+}
