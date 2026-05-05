e21cf6f30ed613b9694c63e79f411322de552355
Jonathan Romero <109704905+yehodarlyn@users.noreply.github.com>
2026-05-04 23:24:10 -0700

    Pantalla de abandonar

--- cliente_UNO/src/main/java/com/mycompany/cliente_uno/Cliente_UNO.java
+++ cliente_UNO/src/main/java/com/mycompany/cliente_uno/Cliente_UNO.java
@@ -4,13 +4,27 @@
 
 package com.mycompany.cliente_uno;
 
+import vista.PantallaAbandonar;
+import javax.swing.SwingUtilities;
+
 /**
  *
  * @author ReneEzequiel23 & EdgarAcevedoAcosta
  */
+
+
+
 public class Cliente_UNO {
 
     public static void main(String[] args) {
+
         System.out.println("Hello World!");
+
+        SwingUtilities.invokeLater(() -> {
+
+            PantallaAbandonar pantalla = new PantallaAbandonar(null);
+            pantalla.setVisible(true);
+
+        });
     }
 }
