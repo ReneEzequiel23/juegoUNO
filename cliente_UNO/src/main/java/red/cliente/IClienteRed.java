585b28ab8c589de11ae7a38328dba9ae541c636d
Adrián Castán <151794030+adcastan@users.noreply.github.com>
2026-05-08 23:41:15 -0700

    cambio de red

---
+++ cliente_UNO/src/main/java/red/cliente/IClienteRed.java
@@ -0,0 +1,17 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Interface.java to edit this template
+ */
+package red.cliente;
+
+import dtos.ComandoJugadorDTO;
+
+/**
+ *
+ * @author Adrián
+ */
+public interface IClienteRed {
+
+    void enviarComando(ComandoJugadorDTO comando);
+
+}
