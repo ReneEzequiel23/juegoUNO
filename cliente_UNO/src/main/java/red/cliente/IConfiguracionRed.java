5ecadcf49776d2e318af3785222c36d6ecec6580
Adrián Castán <151794030+adcastan@users.noreply.github.com>
2026-05-08 23:19:13 -0700

    clienteUNO

---
+++ cliente_UNO/src/main/java/red/cliente/IConfiguracionRed.java
@@ -0,0 +1,16 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Interface.java to edit this template
+ */
+package red.cliente;
+
+import dtos.EstadoConfiguracionDTO;
+
+/**
+ *
+ * @author Adrián
+ */
+public interface IConfiguracionRed {
+       void enviarComando(EstadoConfiguracionDTO comando);
+ 
+}
--- comunes_UNO/src/main/java/dtos/EstadoConfiguracionDTO.java
+++ comunes_UNO/src/main/java/dtos/EstadoConfiguracionDTO.java
@@ -12,4 +12,61 @@ import java.io.Serializable;
  */
 public class EstadoConfiguracionDTO implements Serializable{
     
+    //Host de la partida
+    /**
+     * cartasMano número máximo de cartas en la mano actuales
+     * 
+     */
+    private String idHost; 
+    private int cartasMano;
+    private int tiempoTotal;
+    private int numJugadores;
+
+    /**
+     * 
+     * @param idHost Host de la partida
+     * @param cartasMano  número máximo de cartas en la mano actuales
+     * @param tiempoTotal segundos actuales máximos del turno de un jugador
+     * @param numJugadores cantidad máxima de jugadores en una partida
+     */
+    public EstadoConfiguracionDTO(String idHost, int cartasMano, int tiempoTotal, int numJugadores) {
+        this.idHost = idHost;
+        this.cartasMano = cartasMano;
+        this.tiempoTotal = tiempoTotal;
+        this.numJugadores = numJugadores;
+    }
+
+    public String getIdHost() {
+        return idHost;
+    }
+
+    public void setIdHost(String idHost) {
+        this.idHost = idHost;
+    }
+
+    public int getCartasMano() {
+        return cartasMano;
+    }
+
+    public void setCartasMano(int cartasMano) {
+        this.cartasMano = cartasMano;
+    }
+
+    public int getTiempoTotal() {
+        return tiempoTotal;
+    }
+
+    public void setTiempoTotal(int tiempoTotal) {
+        this.tiempoTotal = tiempoTotal;
+    }
+
+    public int getNumJugadores() {
+        return numJugadores;
+    }
+
+    public void setNumJugadores(int numJugadores) {
+        this.numJugadores = numJugadores;
+    }
+    
+    
 }
