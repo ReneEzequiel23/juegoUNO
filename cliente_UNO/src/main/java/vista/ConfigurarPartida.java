11302111a186a64ccdca1991894e0e811c11f3b5
Adrián Castán <151794030+adcastan@users.noreply.github.com>
2026-05-16 15:19:25 -0700

    update - Adrián

--- cliente_UNO/src/main/java/vista/ConfigurarPartida.java
+++ cliente_UNO/src/main/java/vista/ConfigurarPartida.java
@@ -28,9 +28,6 @@ public class ConfigurarPartida extends javax.swing.JFrame {
         initComponents();
     }
 
-    JComboBox<String> combo = (JComboBox<String>) getContentPane().getComponent(0);
-
-
     /**
      * This method is called from within the constructor to initialize the form.
      * WARNING: Do NOT modify this code. The content of this method is always
